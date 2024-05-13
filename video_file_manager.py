38556a023b707a58c49d2f01948e8ed41b1a7631
noahmagcalas <noah.magcalas@gmail.com>
2024-05-12 19:32:46 -0500

    made video renamer rename videos

--- video_file_manager.py
+++ video_file_manager.py
@@ -2,21 +2,38 @@
 import os
 import re
 
-os.chdir("B:\\videos\\recordings")
+# MM-DD-YYYY -> YYYY-MM-DD
+def year_to_front(date: str) -> str:
+    date_tokens = date.split('-')
+    date_tokens.insert(0, date_tokens.pop())
+    return '-'.join(date_tokens)
 
-bad_date_pattern = "\d\d-\d\d-\d\d\d\d"
+def get_file_path() -> str:
+    input_path = ''
+    while not os.path.exists(input_path):
+        input_path = input('Enter a file path: ')
+    return input_path
+
+
+bad_date_pattern = r"\d\d-\d\d-\d\d\d\d"
 processed_file_names = {}
 
-for dir_path, dir_names, file_names in os.walk('.'):
-    for f in file_names:
-        if re.search(bad_date_pattern, f):
-            split_by_space = f.split(' ')
-            date_tokens = split_by_space[0].split('-')
+if __name__ == '__main__':
+    path_to_search = get_file_path()
+    # path_to_search = 'test_files'
+    for root, dirs, files in os.walk(path_to_search):
+        for name in files:
+            if not name.endswith(('.mkv', '.mp4')):
+                continue
+
+            date_match = re.search(bad_date_pattern, name)
+            if not date_match:
+                continue
+            reformatted_date = year_to_front(date_match.group())
             
-            # move year to front
-            date_tokens.insert(0, date_tokens.pop())
-            reformatted_date = '-'.join(date_tokens)
+            # print(name.replace(date_match.group(), reformatted_date))
+            old_path = os.path.join(root, name)
+            new_path = os.path.join(root, name.replace(date_match.group(), reformatted_date))
+            os.rename(old_path, new_path)
             
-            print(split_by_space[0], '->', reformatted_date)
-            
-        # find duplicate files of different type
+            # find duplicate files of different type
